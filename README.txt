9690be071502c79065e77fd345df2df4b945d2f1
Seunghwan Jeon <fy16sj@leeds.ac.uk>
2020-03-18 11:41:48 +0000

    Initial commit

---
+++ README.txt
@@ -0,0 +1,43 @@
+To use the client, simply run the python program client.py:
+python client.py
+Then the command line interface is shown as the following: >>> 
+
+There are 8 commands in total
+
+1. register
+- Register the new user.
+- Simply type 'register' on command line interface
+
+2. login 'url/'
+- Log the user in
+- Simply type 'login url' on command line interface, where url is the URL of server (please put '/' at the end of url, e.g. https://fy16sj.pythonanywhere.com/)
+
+3. logout
+- Log the user out
+- Simply type 'logout' on command line interface
+
+4. list
+- List the information of module instances
+- Simply type 'list' on command line interface
+
+5. view
+- View the ratings of professors
+- Simply type 'view' on command line interface
+
+6. average 'professor id' 'module code'
+- View the average of professor in a certain module instance
+- Simply type 'register professor_id module_code' on command line interface, where professor_id is the unique id of professor and module_code is the unique code of module.
+
+7. rate 'professor id' 'module code' 'year' 'semester' 'rating'
+- Rate the professor
+- Simply type the command as shown above, where each parameter is self-explanatory.
+
+8. quit
+- Terminate the program
+- Simply type 'register' on command line interface
+
+
+The name of pythonanywhere.com domain is https://fy16sj.pythonanywhere.com
+The password to be used for admin site is abcd1234.
+
+*The PrettyTable package has been used for the list command, so please install this!
---
+++ client.py
@@ -0,0 +1,101 @@
+import requests
+from prettytable import PrettyTable
+
+
+url = 'https://fy16sj.pythonanywhere.com/'
+
+# use for loop to find all the titles of modules
+# list function
+def list():
+    output = requests.get(url + 'list').text
+    print(output)
+
+
+def view():
+    output = requests.get(url + 'professor/view').text
+    print(output)
+
+def rate(profId, modCode, y, term, rating):
+    # if url == '':
+    #     return print("Log in first!")
+    info = {'pro_id':profId, 'm_code':modCode, 'y':y, 'term':term, 'rating':rating}
+    requests.post(url + 'rate', data=info)
+    # print(response.text)
+
+
+def average(professorId, moduleId):
+    # if url == '':
+    #     return print("Log in first!")
+    average = requests.get(url + 'rate/average/%s/%s' % (moduleId, professorId)).text
+    if average == -1:
+        return print("Cannot find the rating for this module and professor")
+    professor_name = requests.get(url + 'professor/code/name/%s' % professorId).text
+    if professor_name == -1:
+        return print("Cannot find professor name")
+    module_name = requests.get(url + 'module/name/%s' % moduleId).text
+    if module_name == -1:
+        return print("Cannot find module names")
+    print('\nThe rating of ' + professor_name + ' (' + professorId + ')' + ' in module ' + module_name + ' (' + moduleId + ')' + ' is ' + '*'*int(average))
+
+
+def register(username, email, password):
+    info = {'username': username, 'email': email, 'password': password}
+    response = requests.post(url + 'register', data=info)
+    print(response.text)
+
+def login(login_url, username, password):
+    info = {'username': username, 'password': password}
+    response = requests.post(('https://' + login_url + '/log_in'), data=info)
+    print(response.text)
+
+
+def logout():
+    response = requests.get(url + 'log_out')
+    print(response.text)
+
+def main():
+    while(1):
+        inputlist = input('>>> ')
+        cmd = inputlist.split()
+        if cmd[0] == 'register':
+            userId = input('Username: ')
+            e_mail = input('Email: ')
+            passcode = input('Password: ')
+            register(userId, e_mail, passcode)
+        elif cmd[0] == 'login':
+            if len(cmd) == 1:
+                print("Wrong Input!")
+            elif len(cmd) > 2:
+                print("Wrong Input!")
+            else:
+                userId = input('Username: ')
+                passcode = input('Password: ')
+                login(cmd[1], userId, passcode)
+        elif cmd[0] == 'logout':
+            logout()
+        elif cmd[0] == 'list':
+            list()
+        elif cmd[0] == 'view':
+            view()
+        elif cmd[0] == 'average':
+            if len(cmd) <= 2:
+                print("Wrong Input!")
+            elif len(cmd) > 3:
+                print("Wrong Input!")
+            else:
+                average(cmd[1], cmd[2])
+        elif cmd[0] == 'rate':
+            if len(cmd) < 6:
+                print("Wrong Input!")
+            elif len(cmd) > 6:
+                print("Wrong Input!")
+            else:
+                rate(cmd[1], cmd[2], cmd[3], cmd[4], cmd[5])
+        elif cmd[0] == 'quit':
+            break
+        else:
+            print("Wrong Input!")
+
+if __name__ == '__main__':
+    main()
+
